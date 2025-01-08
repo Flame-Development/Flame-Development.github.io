16ab3bf6025d0c11931847d55a4782c3026babf9
Mehran1022 <new.dev1022@gmail.com>
2025-01-08 22:48:38 +0330

    Initial Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 function clearInput() {
     document.getElementById("message").value = '';
 }
+
+function submit() {
+    var button = document.getElementById("send");
+    button.style.transition = "opacity 0.3s, color 0.3s";
+    button.style.opacity = 0;
+
+    setTimeout(function () {
+        button.innerHTML = "Done!";
+        button.style.backgroundColor = "#4ec032c5";
+        button.style.opacity = 1;
+
+        setTimeout(function () {
+            button.style.opacity = 0;
+
+            setTimeout(function () {
+                button.innerHTML = "Send";
+                button.style.backgroundColor = "";
+                button.style.opacity = 1;
+            }, 500);
+        }, 2500);
+    }, 500);
+}
